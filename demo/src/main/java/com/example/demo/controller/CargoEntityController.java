7f27c6b59c29061fcf36fd4059c673c39dd3f3e5
Maciej Kłębowski <klebowskimaciej@gmail.com>
2022-05-04 13:58:58 +0200

    r work

--- demo/src/main/java/com/example/demo/controller/CargoEntityController.java
+++ demo/src/main/java/com/example/demo/controller/CargoEntityController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.CargoEntity;
+import com.example.demo.service.CargoEntityService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +10,13 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class CargoEntityController {
 
+    private final CargoEntityService cargoEntityService;
     @GetMapping("/CargoEntity")
     private List<CargoEntity> getCargoEntity(){
-        throw new IllegalArgumentException("Not implemented yet!");
+        return  cargoEntityService.getCargoEntity();
 
     }
     @GetMapping("/CargoEntity/{flight_id}")
--- demo/src/main/java/com/example/demo/service/CargoEntityService.java
+++ demo/src/main/java/com/example/demo/service/CargoEntityService.java
@@ -1,6 +1,11 @@
 package com.example.demo.service;
 
+import com.example.demo.model.CargoEntity;
+import com.example.demo.repository.CargoEntityRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /*
 W przypadku żądanego numeru lotu i daty poda następujące informacje:
@@ -9,6 +14,14 @@ b. Waga bagażu dla żądanego lotu
 c. Waga całkowita dla żądanego lotu
  */
 @Service
+@RequiredArgsConstructor
 public class CargoEntityService {
+
+    private final CargoEntityRepository cargoEntityRepository;
+
+    public List<CargoEntity> getCargoEntity(){
+
+        return cargoEntityRepository.findAll();
+    }
 
 }
